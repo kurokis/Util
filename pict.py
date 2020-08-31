c97e06552ef4be9409e8211c840f183d8e8545de
Shota Kuroki <shota.kuroki.code@gmail.com>
2020-08-31 23:24:38 +0900

    Added N-wise order parameter

--- pict.py
+++ pict.py
@@ -2,6 +2,8 @@ import subprocess
 from pathlib import Path
 import pandas as pd
 
+ORDER = 3
+
 if __name__ == '__main__':
     pict_settings_1 = Path("input_data\\pict_settings_1.txt")
     pict_case_1 = Path("output_data\\pict_case_1.txt")
@@ -12,7 +14,7 @@ if __name__ == '__main__':
     pict_case_2 = Path("output_data\\pict_case_2.txt")
 
     # First PICT case generation
-    command = "pict {} /r:0".format(str(pict_settings_1))
+    command = "pict {} /o:{} /r:0".format(str(pict_settings_1), ORDER)
     with open(str(pict_case_1), mode="w") as f:
         _ = subprocess.run(command, encoding="utf-8", stdout=f)
 
@@ -30,17 +32,17 @@ if __name__ == '__main__':
                     t += line+"\tn\n"
             else:
                 t += line
-        print(t)
         f.write(t)
 
     # Second PICT case generation with seed file
-    command = "pict {} /r:0 /e:{}".format(
-        str(pict_settings_2), str(pict_case_1_5))
+    command = "pict {} /o:{} /r:0 /e:{}".format(
+        str(pict_settings_2), ORDER, str(pict_case_1_5))
     with open(str(pict_case_2), "w") as f:
         _ = subprocess.run(command, encoding="utf-8", stdout=f)
 
     df1 = pd.read_csv(pict_case_1, encoding="utf-8", delimiter="\t")
     df2 = pd.read_csv(pict_case_2, encoding="utf-8", delimiter="\t")
 
+    print(df2)
     print("Combinations in case 1:", len(df1))
     print("Combinations in case 2:", len(df2))
